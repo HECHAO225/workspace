8d28a1e411cfcfabc23884ca17f88006cda3f646
hechao <chaohe003077@sohu-inc.com>
2017-04-28 15:00:55 +0800

    edit resin.py

--- deploy/resin.py
+++ deploy/resin.py
@@ -4,10 +4,10 @@ if len(sys.argv) == 1:
   sys.exit();
 cmd = sys.argv[1];
 if cmd == 'start':
-  os.system('java -jar /opt/apps/resin-pro-4.0.36/lib/resin.jar start --conf /opt/conf/resin/resin.xml');
+  os.system('java -jar /opt/resin-pro-4.0.51/lib/resin.jar start --conf /opt/conf/resin/resin.xml');
 elif cmd == 'stop':
-  os.system('java -jar /opt/apps/resin-pro-4.0.36/lib/resin.jar stop --conf /opt/conf/resin/resin.xml');
+  os.system('java -jar /opt/resin-pro-4.0.51/lib/resin.jar stop --conf /opt/conf/resin/resin.xml');
 elif cmd == 'restart':
-  os.system('java -jar /opt/apps/resin-pro-4.0.36/lib/resin.jar restart --conf /opt/conf/resin/resin.xml');
+  os.system('java -jar /opt/resin-pro-4.0.51/lib/resin.jar restart --conf /opt/conf/resin/resin.xml');
 else:
   print 'nothing happen';
